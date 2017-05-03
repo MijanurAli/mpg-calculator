4f711ff5129a4b79a8fd7aa36cf4e5215ac645c6
Mijanur Ali <mijanur.ali@sabre.com>
2017-05-02 21:22:53 -0500

    Added remaining project files

---
+++ MpgCalculator/frmMpgCalculator.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtTotalGals = New System.Windows.Forms.TextBox()
+        Me.txtTotalMiles = New System.Windows.Forms.TextBox()
+        Me.txtTotalMpg = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(95, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(146, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Gallons Vehicle Holds"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(80, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(161, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Total Miles On Full Tank"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(166, 145)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(75, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Total MPG"
+        '
+        'txtTotalGals
+        '
+        Me.txtTotalGals.Location = New System.Drawing.Point(272, 36)
+        Me.txtTotalGals.Name = "txtTotalGals"
+        Me.txtTotalGals.Size = New System.Drawing.Size(100, 22)
+        Me.txtTotalGals.TabIndex = 3
+        '
+        'txtTotalMiles
+        '
+        Me.txtTotalMiles.Location = New System.Drawing.Point(272, 87)
+        Me.txtTotalMiles.Name = "txtTotalMiles"
+        Me.txtTotalMiles.Size = New System.Drawing.Size(100, 22)
+        Me.txtTotalMiles.TabIndex = 4
+        '
+        'txtTotalMpg
+        '
+        Me.txtTotalMpg.Location = New System.Drawing.Point(272, 145)
+        Me.txtTotalMpg.Name = "txtTotalMpg"
+        Me.txtTotalMpg.ReadOnly = True
+        Me.txtTotalMpg.Size = New System.Drawing.Size(100, 22)
+        Me.txtTotalMpg.TabIndex = 5
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(59, 219)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(85, 29)
+        Me.btnCalculate.TabIndex = 6
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(195, 219)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(86, 27)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(329, 219)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(86, 27)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(459, 281)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtTotalMpg)
+        Me.Controls.Add(Me.txtTotalMiles)
+        Me.Controls.Add(Me.txtTotalGals)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Mpg Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtTotalGals As TextBox
+    Friend WithEvents txtTotalMiles As TextBox
+    Friend WithEvents txtTotalMpg As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ MpgCalculator/frmMpgCalculator.vb
@@ -0,0 +1,38 @@
+﻿Imports VBClassLibrary
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Set the focus for txtTotalGals
+        txtTotalGals.Focus()
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'Variables for text boxes to hold values
+        Dim totGals As Decimal = CDbl(txtTotalGals.Text)
+        Dim numMiles As Decimal = CDbl(txtTotalMiles.Text)
+
+        If IsValidData() Then
+            ' Calculate the total MPG
+            txtTotalMpg.Text = numMiles / totGals
+        Else
+            MessageBox.Show("You didn't enter a valid number. Try again", "Input Error")
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Clear all text boxes
+        txtTotalGals.Clear()
+        txtTotalMiles.Clear()
+        txtTotalMpg.Clear()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close the window
+        Me.Close()
+    End Sub
+
+    Private Function IsValidData() As Boolean
+        Return WindowsValidator.IsDouble(txtTotalGals) AndAlso
+            WindowsValidator.IsDouble(txtTotalMiles)
+    End Function
+End Class
